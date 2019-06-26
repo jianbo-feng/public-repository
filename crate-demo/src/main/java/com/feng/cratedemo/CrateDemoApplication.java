c8441a82f81be74e74241c6ac466e054f3b4a6df
jianbo.feng <445121408@qq.com>
2019-06-26 23:23:16 +0800

    2019/06/26 commit

---
+++ crate-demo/src/main/java/com/feng/cratedemo/CrateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.feng.cratedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrateDemoApplication.class, args);
+	}
+
+}
